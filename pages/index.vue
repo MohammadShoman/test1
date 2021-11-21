d9039aab0fb0edd859ec6358a34774589c3d4c97
abdalafo98 <abdalafo.abubaker@gmail.com>
2021-11-21 12:35:54 +0200

    test frontend

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,56 @@
 <template>
   <div>
-    <h2>A basic HTML table</h2>
-
-    <table style="width: 100%">
+    <h2>Calender</h2>
+    <table width="100%">
       <tr>
-        <th>Company</th>
-        <th>Contact</th>
-        <th>Country</th>
+        <th></th>
+        <th :key="day" v-for="day in days">
+          {{ day }}
+        </th>
       </tr>
       <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
+        <th :key="doctor" v-for="doctor in data"></th>
+        <td>{{ doctor.days }}</td>
+        <td></td>
       </tr>
       <tr>
-        <td>Centro comercial Moctezuma</td>
-        <td>Francisco Chang</td>
-        <td>Mexico</td>
+        <th>Susan</th>
+        <td>23 km</td>
+        <td>18 km</td>
+        <td>19 km</td>
+        <td>15 km</td>
       </tr>
     </table>
-
     <p>To undestand the example better, we have added borders to the table.</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      days: ["SAT", "SUN", "MON", "TUS", "WED", "THU", "FRI"],
+      data: [],
+    };
+  },
+  async created() {
+    const response = await fetch(
+      "https://619355dcd3ae6d0017da84c7.mockapi.io/doctors"
+    );
+    const data = await response.json();
+    this.data = data;
+    console.log(data);
+  },
+  methods: {
+    async getData() {},
+  },
+};
 </script>
 
 <style>
 table,
 th,
 td {
-  border: 1px solid black;
+  border: 1px solid rgb(36, 110, 252);
 }
 </style>
